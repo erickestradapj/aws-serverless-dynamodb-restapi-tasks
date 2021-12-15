e753fc1a20450b28ffff93d80a897f501d507a5e
Erick Estrada <erickestradapj@gmail.com>
2021-12-14 23:50:04 -0500

    get task

---
+++ src/getTasks.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+
+const getTasks = async (event) => {
+   const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+   const result = await dynamodb
+      .scan({
+         TableName: 'TaskTable',
+      })
+      .promise();
+
+   const tasks = result.Items;
+
+   console.log(tasks);
+
+   return {
+      status: 200,
+      body: {
+         tasks,
+      },
+   };
+};
+
+module.exports = {
+   getTasks,
+};
