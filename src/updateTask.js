aeef23ca337d51517be7182ffa5d467a97e00079
Erick Estrada <erickestradapj@gmail.com>
2021-12-15 00:44:21 -0500

    update task

--- src/updateTask.js
+++ src/updateTask.js
@@ -3,15 +3,17 @@ const AWS = require('aws-sdk');
 const updateTask = async (event) => {
    const dynamodb = new AWS.DynamoDB.DocumentClient();
    const { id } = event.pathParameters;
-   const { done } = JSON.parse(event.body);
+   const { done, title, description } = JSON.parse(event.body);
 
    await dynamodb
       .update({
          TableName: 'TaskTable',
          Key: { id },
-         UpdateExpression: 'set done = :done',
+         UpdateExpression: 'set done = :done, title = :title, description = :description',
          ExpressionAttributeValues: {
             ':done': done,
+            ':title': title,
+            ':description': description,
          },
          ReturnValues: 'ALL_NEW',
       })
